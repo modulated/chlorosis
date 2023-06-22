108ad7f6ad6f3d56d90545dba4fe502132c65cb3
George Roubos <cowkingdom@hotmail.com>
2023-06-22 11:20:53 +1000

    Update constants.rs

--- src/emu/memory/constants.rs
+++ src/emu/memory/constants.rs
@@ -1,8 +1,11 @@
 // MEMORY ADDRESS POSITION CONSTANTS
-pub const CARTRIGE_0_START: u16 = 0x0000;
-pub const CARTRIGE_0_END: u16 = 0x3FFF;
-pub const CARTRIGE_1_START: u16 = 0x4000;
-pub const CARTRIGE_1_END: u16 = 0x7FFF;
+pub const BOOT_ROM_START: u16 = 0x0000;
+pub const BOOT_ROM_END: u16 = 0x00FF;
+pub const CARTRIGE_ENTRY: u16 = 0x0100;
+pub const ROM_0_START: u16 = 0x0000;
+pub const ROM_0_END: u16 = 0x3FFF;
+pub const ROM_1_START: u16 = 0x4000;
+pub const ROM_1_END: u16 = 0x7FFF;
 pub const VRAM_START: u16 = 0x8000;
 pub const VRAM_END: u16 = 0x9FFF;
 pub const ERAM_START: u16 = 0xA000;
@@ -29,7 +32,7 @@ pub const WRAM_BANK_SIZE: usize = 0x1000; // 4 KB
 pub const VRAM_SIZE: usize = 0x4000; // 16 KB
 pub const VRAM_BANK_SIZE: usize = 0x2000; // 8 KB
 pub const ERAM_SIZE: usize = 0x2000; // 8 KB - TODO: ERAM mapper
-pub const CARTRIGE_BANK_SIZE: usize = 0x4000; // 16 KB
+pub const ROM_BANK_SIZE: usize = 0x4000; // 16 KB
 pub const OAM_SIZE: usize = 0xA0; // 160
 pub const HRAM_SIZE: usize = 0x7F; // 127
 pub const IO_SIZE: usize = 0x80; // 128
