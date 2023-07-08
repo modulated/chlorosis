9dfd48a0abedbd3fc869cc3319c3cec6b1035572
George Roubos <cowkingdom@hotmail.com>
2023-07-08 23:47:34 +1000

    add debug pausing

--- debugger/src/main.rs
+++ debugger/src/main.rs
@@ -1,54 +1,40 @@
-use std::{sync::mpsc::Sender, time::Duration};
+use std::{
+    sync::mpsc::{Receiver, Sender},
+    thread,
+    time::Duration,
+};
 
 use chlorosis_core::{Device, Event, KeyCode};
 use minifb::{Key, Menu, Window, WindowOptions, MENU_KEY_CTRL};
 
 const WIDTH: usize = 160;
 const HEIGHT: usize = 144;
+const BLACK: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
+const GREY: [u32; WIDTH * HEIGHT] = [0x5555; WIDTH * HEIGHT];
 
 fn main() {
     let mut dev = Device::default();
+    let mut state = DebuggerState::Stopped;
 
     let mut window = build_window();
 
     let (buffer_sender, buffer_receiver) = std::sync::mpsc::channel();
     let (event_sender, event_receiver) = std::sync::mpsc::channel();
 
-    const BLACK: [u32; WIDTH * HEIGHT] = [0; WIDTH * HEIGHT];
-
     std::thread::Builder::new()
         .name("Core".to_owned())
         .spawn(move || dev.run(buffer_sender, event_receiver))
         .unwrap();
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        if let Ok(b) = buffer_receiver.try_recv() {
-            window.update_with_buffer(&b, WIDTH, HEIGHT).unwrap();
-        } else {
-            window.update_with_buffer(&BLACK, WIDTH, HEIGHT).unwrap();
+    while window.is_open() && state != DebuggerState::Quitting {
+        match state {
+            DebuggerState::Running => running(&mut window, &buffer_receiver, &event_sender),
+            DebuggerState::Stopped => stopped(&mut window),
+            DebuggerState::Paused => paused(&mut window),
+            DebuggerState::Quitting => unreachable!("Cannot be quiting in loop"),
         }
 
-        let keys_down: Vec<KeyCode> = window
-            .get_keys_pressed(minifb::KeyRepeat::No)
-            .iter()
-            .filter_map(key_to_keycode)
-            .collect();
-        if !keys_down.is_empty() {
-            event_sender.send(Event::KeyDown(keys_down)).unwrap();
-        }
-
-        let keys_up: Vec<KeyCode> = window
-            .get_keys_released()
-            .iter()
-            .filter_map(key_to_keycode)
-            .collect();
-        if !keys_up.is_empty() {
-            event_sender.send(Event::KeyUp(keys_up)).unwrap();
-        }
-
-        if let Some(n) = window.is_menu_pressed() {
-            handle_menu(n, &event_sender)
-        }
+        handle_debugger_input(&mut window, &mut state, &event_sender);
     }
 
     event_sender.send(Event::Exit).unwrap();
@@ -101,7 +87,39 @@ fn key_to_keycode(k: &Key) -> Option<KeyCode> {
     }
 }
 
-fn handle_menu(menu: usize, sender: &Sender<Event>) {
+fn handle_debugger_input(
+    window: &mut Window,
+    state: &mut DebuggerState,
+    event_sender: &Sender<Event>,
+) {
+    if window.is_key_down(Key::Escape) {
+        *state = DebuggerState::Quitting;
+    }
+
+    if let Some(n) = window.is_menu_pressed() {
+        handle_menu(n, event_sender, state);
+    }
+
+    if window.is_key_released(Key::Space) {
+        dbg!(*state);
+        match state {
+            DebuggerState::Running => {
+                println!("Pausing!");
+                event_sender.send(Event::Pause).unwrap();
+                *state = DebuggerState::Paused;
+            }
+            DebuggerState::Paused => {
+                println!("Resuming!");
+                event_sender.send(Event::Run).unwrap();
+                *state = DebuggerState::Running;
+            }
+            _ => {}
+        }
+        thread::sleep(Duration::from_millis(50));
+    }
+}
+
+fn handle_menu(menu: usize, sender: &Sender<Event>, state: &mut DebuggerState) {
     match menu {
         1 => {
             let f = native_dialog::FileDialog::new()
@@ -110,6 +128,7 @@ fn handle_menu(menu: usize, sender: &Sender<Event>) {
                 .unwrap();
             if let Some(f) = f {
                 sender.send(Event::LoadFile(f)).unwrap();
+                *state = DebuggerState::Running;
             }
         }
         2 => {
@@ -117,4 +136,52 @@ fn handle_menu(menu: usize, sender: &Sender<Event>) {
         }
         _ => println!("Unhandled menu {menu}"),
     }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum DebuggerState {
+    Stopped,
+    Running,
+    Paused,
+    Quitting,
+}
+
+fn running(
+    window: &mut Window,
+    buffer_receiver: &Receiver<Vec<u32>>,
+    event_sender: &Sender<Event>,
+) {
+    if let Ok(b) = buffer_receiver.try_recv() {
+        window.update_with_buffer(&b, WIDTH, HEIGHT).unwrap();
+    } else {
+        window.update_with_buffer(&BLACK, WIDTH, HEIGHT).unwrap();
+    }
+
+    let keys_down: Vec<KeyCode> = window
+        .get_keys_pressed(minifb::KeyRepeat::No)
+        .iter()
+        .filter_map(key_to_keycode)
+        .collect();
+    if !keys_down.is_empty() {
+        event_sender.send(Event::KeyDown(keys_down)).unwrap();
+    }
+
+    let keys_up: Vec<KeyCode> = window
+        .get_keys_released()
+        .iter()
+        .filter_map(key_to_keycode)
+        .collect();
+    if !keys_up.is_empty() {
+        event_sender.send(Event::KeyUp(keys_up)).unwrap();
+    }
+}
+
+fn paused(window: &mut Window) {
+    window.update_with_buffer(&GREY, WIDTH, HEIGHT).unwrap();
+    std::thread::sleep(Duration::from_millis(33));
+}
+
+fn stopped(window: &mut Window) {
+    window.update_with_buffer(&BLACK, WIDTH, HEIGHT).unwrap();
+    std::thread::sleep(Duration::from_millis(33));
 }
